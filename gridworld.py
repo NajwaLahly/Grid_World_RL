c437ec12fae5ed6cb0ae932d0070ca0d92761314
Najwa Lahly <najwa.lahly@gmail.com>
2020-06-22 14:42:25 +0200

    function renamed+ remove previous sprite position

--- gridworld.py
+++ gridworld.py
@@ -1,10 +1,7 @@
 import sys
-import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.table import Table
 import pygame
 import pdb
-from pprint import pprint
 
 # global variables
 BOARD_ROWS = 3
@@ -33,7 +30,7 @@ class State:
         if self.state == WIN_STATE or self.state == LOSE_STATE:
             self.end = True
 
-    def giveReward(self, state):
+    def give_reward(self, state):
         if state == WIN_STATE:
             return 1
         elif state == LOSE_STATE:
@@ -41,7 +38,7 @@ class State:
         else:
             return 0
 
-    def nextPosition(self, action):
+    def next_position(self, action):
         if not self.end:
             if action == "up":
                 nxtState = (self.state[0] - 1, self.state[1])
@@ -60,12 +57,11 @@ class State:
                 return self.state
             return self.state
 
-        # else:
-        # TODO
-        # pass
+        else:
+            return self.state
 
-    def actionIsLegal(self, action):
-        if self.nextPosition(action) == self.state:
+    def action_is_legal(self, action):
+        if self.next_position(action) == self.state:
             return False
         return True
 
@@ -77,15 +73,15 @@ class Agent:
         self.actions = ["up", "down", "left", "right"]
         self.exp_rate = 0.2
         self.state_values = {}
-        self.initializeRewards()  # initializing rewards
+        self.initialize_rewards()  # initializing rewards
 
-    def initializeRewards(self):
+    def initialize_rewards(self):
         for i in range(BOARD_ROWS):
             for j in range(BOARD_COLS):
                 self.state_values[(i, j)] = 0
 
-    def giveprobabilities(self, action, otheractions):
-        if not self.State.actionIsLegal(action):
+    def give_probabilities(self, action, otheractions):
+        if not self.State.action_is_legal(action):
             return 0
         else:
             if otheractions == action:
@@ -93,43 +89,38 @@ class Agent:
             else:
                 return 0.1
 
-    def getvalue(self):
+    def get_value(self):
 
         if self.State.state == WIN_STATE or self.State.state == LOSE_STATE or self.State.state == WALL_STATE:
-            value = self.State.giveReward(self.State.state)
+            value = self.State.give_reward(self.State.state)
 
         else:
             nxt_value = []
             for a in self.actions:
                 E = 0
                 for act in self.actions:
-                    E += self.giveprobabilities(act, a) * self.state_values[self.State.nextPosition(act)]
-                nxt_value.append(self.State.giveReward(self.State.state) + discount * E)
+                    E += self.give_probabilities(act, a) * self.state_values[self.State.next_position(act)]
+                nxt_value.append(self.State.give_reward(self.State.state) + discount * E)
                 max_value_idx = nxt_value.index(max(nxt_value))
                 value = max(nxt_value)
         return value
-
-    def takeAction(self, action):
-        position = self.State.nextPosition(action)
-        return State(state=position)
 
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, world):
-        # self.world = world
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30, 30))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         sub = tuple(sum(i) for i in zip(world.blockSize * np.array(START), (world.blockSize / 2, world.blockSize / 2)))
         self.rect.center = (sub[1], sub[0])
-        # pdb.set_trace()
         self.position = START
 
     def update(self):
-        new_move = world.agent.State.nextPosition(world.move_player_direction(self.position))
-        self.rect.center = (new_move[1] * world.blockSize + world.blockSize / 2, new_move[0] * world.blockSize + world.blockSize / 2)
+        new_move = world.agent.State.next_position(world.take_action(self.position))
+        self.rect.center = (
+            new_move[1] * world.blockSize + world.blockSize / 2, new_move[0] * world.blockSize + world.blockSize / 2)
         self.position = new_move
 
 
@@ -148,16 +139,40 @@ class World:
         self.init_game()
         self.clock = pygame.time.Clock()
 
+    def value_iteration_loop(self):
+
+        k = 0
+        while True:
+            values = {}
+            newWorld = np.zeros([BOARD_ROWS, BOARD_COLS])
+            for (i, j), val in np.ndenumerate(self.board):
+                self.agent.State.state = (i, j)
+                newWorld[i, j] = self.agent.get_value()
+                values[(i, j)] = newWorld[i, j]
+
+            self.agent.state_values = values
+            if np.sum(np.abs(self.board - newWorld)) < 1e-3:
+                break
+
+            self.board = newWorld
+            k += 1
+        return self.board
+
+    def take_action(self, state):
+        direction_values = []
+        for a in self.agent.actions:
+            self.agent.State.state = state
+            potential_pos = self.agent.State.next_position(a)
+            direction_values.append(self.board[potential_pos])
+            idx_max_direction = np.argmax(direction_values)
+            take_action = self.agent.actions[idx_max_direction]
+        return take_action
+
     def init_game(self):
         pygame.init()
         pygame.display.set_caption("GridWorld")
-        self.drawGrid()
 
-    def reset(self):
-        self.states = []
-        self.agent.State = State()
-
-    def drawGrid(self):
+    def draw_grid(self):
 
         for i in range(BOARD_ROWS):
             for j in range(BOARD_COLS):
@@ -170,64 +185,33 @@ class World:
                 elif (i, j) == WALL_STATE:
                     pygame.draw.rect(self.SCREEN, WHITE, rect, 0)
                 pygame.draw.rect(self.SCREEN, WHITE, rect, 1)
+        return self.SCREEN
 
     def add_player(self, player):
         self.all_sprites.add(player)
 
     def start_game(self):
-        k = 0
+
         while self.running:
+
             self.clock.tick(FPS)
+            self.SCREEN.fill(BLACK)
+            self.draw_grid()
             for event in pygame.event.get():
-                if event.type == pygame.QUIT or self.agent.State.end == True:
+                if event.type == pygame.QUIT:
                     self.running = False
                     pygame.quit()
                     sys.exit()
-            # self.SCREEN.fill(BLACK)
+
             self.all_sprites.draw(self.SCREEN)
             self.all_sprites.update()
             pygame.display.flip()
-            k += 1
-            if k == 0:
-                pdb.set_trace()
         pygame.quit()
-
-    def play(self):
-
-        k = 0
-        while True:
-            values = {}
-            newWorld = np.zeros([BOARD_ROWS, BOARD_COLS])
-            for (i, j), val in np.ndenumerate(self.board):
-                self.agent.State.state = (i, j)
-                newWorld[i, j] = self.agent.getvalue()
-                values[(i, j)] = newWorld[i, j]
-
-            self.agent.state_values = values
-            if np.sum(np.abs(self.board - newWorld)) < 1e-3:
-                # self.agent.State.end = True
-                break
-
-            self.board = newWorld
-            k += 1
-        return self.board
-
-    def move_player_direction(self, state):
-        direction_values = []
-        for a in self.agent.actions:
-            # pdb.set_trace()
-            self.agent.State.state = state
-            # pdb.set_trace()
-            potential_pos = self.agent.State.nextPosition(a)
-            direction_values.append(self.board[potential_pos])
-            idx_max_direction = np.argmax(direction_values)
-            # pdb.set_trace()
-        return self.agent.actions[idx_max_direction]
 
 
 if __name__ == "__main__":
     world = World()
-    world.play()
+    world.value_iteration_loop()
     player = Player(world)
     world.add_player(player)
     world.start_game()
